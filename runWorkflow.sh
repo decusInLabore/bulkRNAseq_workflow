9a5ea3abbbbb48b25524e07cc732f1ee504759b6
Stefan Boeing <boeings@babs001.camp.thecrick.org>
2022-05-25 07:43:29 +0100

    Added run shell script

--- runWorkflow.sh
+++ runWorkflow.sh
@@ -22,5 +22,7 @@ wait_on_lsf
 project=rB_$project_id
 sbatch --time=06:00:00 --wrap "module purge;source /camp/stp/babs/working/software/modulepath_new_software_tree_2018-08-13;module load pandoc/2.2.3.2-foss-2016b;ml R/4.0.3-foss-2020a;Rscript runRmd.r PartB_Analysis.Rmd" --job-name=$project -p hmem --mem=300G -o ../rA.$project.slurm >> ../commands.txt
 
+wait_on_lsf
+
 project=rC_$project_id
 sbatch --time=06:00:00 --wrap "module purge;source /camp/stp/babs/working/software/modulepath_new_software_tree_2018-08-13;module load pandoc/2.2.3.2-foss-2016b;ml R/4.0.3-foss-2020a;Rscript runRmd.r PartB_Analysis.Rmd" --job-name=$project -p hmem --mem=300G -o ../rA.$project.slurm >> ../commands.txt
